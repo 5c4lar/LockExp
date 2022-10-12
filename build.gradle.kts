737e502aea8181dc06079c0342113b77b80d5094
ucasqsl <qinsiliang18@mails.ucas.ac.cn>
2022-10-12 09:23:53 +0800

    add commandline args

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,35 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.20"
-    java
-    application
+  kotlin("jvm") version "1.7.20"
+  java
+  application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    implementation("org.jetbrains.lets-plot:lets-plot-common:2.5.0")
-    implementation("org.jetbrains.lets-plot:lets-plot-image-export:2.5.0")
-    implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.1.0")
-    implementation("org.slf4j:slf4j-simple:2.0.3")
+  testImplementation(kotlin("test"))
+  implementation("org.jetbrains.lets-plot:lets-plot-common:2.5.0")
+  implementation("org.jetbrains.lets-plot:lets-plot-image-export:2.5.0")
+  implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.1.0")
+  implementation("org.slf4j:slf4j-simple:2.0.3")
+  implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+  kotlinOptions.jvmTarget = "17"
 }
 
 application {
-    mainClass.set("MainKt")
+  mainClass.set("MainKt")
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,313 +1,19 @@
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
+import kotlinx.cli.delimiter
 import org.jetbrains.letsPlot.export.ggsave
 import org.jetbrains.letsPlot.geom.geomPoint
 import org.jetbrains.letsPlot.geom.geomSmooth
 import org.jetbrains.letsPlot.intern.Plot
 import org.jetbrains.letsPlot.letsPlot
-import java.util.concurrent.locks.Lock
-import kotlin.concurrent.thread
 import spin.*
 import java.util.concurrent.CyclicBarrier
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.Lock
 
 //import javaspin.*
-
-//class TASLock :Lock {
-//  private var state: AtomicBoolean = AtomicBoolean(false)
-//
-//  override fun lock() {
-//    while (state.getAndSet(true)) {}
-//  }
-//
-//  override fun unlock() {
-//    state.set(false)
-//  }
-//
-//  override fun lockInterruptibly() {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun tryLock(): Boolean {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun tryLock(time: Long, unit: TimeUnit): Boolean {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun newCondition(): Condition {
-//    TODO("Not yet implemented")
-//  }
-//
-//}
-//
-//class TTASLock :Lock {
-//  private var state: AtomicBoolean = AtomicBoolean(false)
-//
-//  override fun lock() {
-//    while (true) {
-//      while (state.get()) {}
-//      if (!state.getAndSet(true)) {
-//        return
-//      }
-//    }
-//  }
-//
-//  override fun unlock() {
-//    state.set(false)
-//  }
-//
-//  override fun lockInterruptibly() {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun tryLock(): Boolean {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun tryLock(time: Long, unit: TimeUnit): Boolean {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun newCondition(): Condition {
-//    TODO("Not yet implemented")
-//  }
-//
-//}
-//
-//class Backoff(val minDelay:Int, val maxDelay: Int) {
-//  var limit: Int = minDelay
-//
-//  @Throws(InterruptedException::class)
-//  fun backoff() {
-//    val delay = ThreadLocalRandom.current().nextInt(limit)
-//    limit = maxDelay.coerceAtMost(2 * limit)
-//    Thread.sleep(delay.toLong())
-//  }
-//}
-//
-//class BackoffLock(val minDelay:Int, val maxDelay:Int): Lock {
-//  private val state: AtomicBoolean = AtomicBoolean(false)
-//
-//  override fun lock() {
-//    val backoff = Backoff(minDelay, maxDelay)
-//    while (true) {
-//      while(state.get()) {}
-//      if (!state.getAndSet(true)) {
-//        return;
-//      } else {
-//        backoff.backoff()
-//      }
-//    }
-//  }
-//
-//  override fun lockInterruptibly() {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun tryLock(): Boolean {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun tryLock(time: Long, unit: TimeUnit): Boolean {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun unlock() {
-//    state.set(false)
-//  }
-//
-//  override fun newCondition(): Condition {
-//    TODO("Not yet implemented")
-//  }
-//
-//}
-//
-//class ALock(private val capacity:Int) : Lock {
-//  @Volatile
-//  private var flag: BooleanArray = BooleanArray(capacity)
-//  private val mySlotIndex:ThreadLocal<Int> = ThreadLocal.withInitial { 0 }
-//  private val tail: AtomicInteger = AtomicInteger(0)
-//  private val size: Int = capacity
-//  init {
-//    flag[0] = true
-//  }
-//  override fun lock() {
-//    val slot = tail.getAndIncrement() % size
-//    mySlotIndex.set(slot)
-//    while (!flag[slot]) {
-//    }
-//  }
-//
-//  override fun lockInterruptibly() {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun tryLock(): Boolean {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun tryLock(time: Long, unit: TimeUnit): Boolean {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun unlock() {
-//    val slot = mySlotIndex.get()
-//    flag[slot] = false
-//    flag[(slot + 1) % size] = true
-//  }
-//
-//  override fun newCondition(): Condition {
-//    TODO("Not yet implemented")
-//  }
-//
-//}
-//
-//class CLHLock :Lock {
-//  class QNode {
-//    @Volatile
-//    var locked: Boolean = false
-//  }
-//  private val tail: AtomicReference<QNode> = AtomicReference(QNode())
-//  private var myPred: ThreadLocal<QNode> = ThreadLocal.withInitial { null }
-//  private var myNode: ThreadLocal<QNode> = ThreadLocal.withInitial { QNode() }
-//  override fun lock() {
-//    val qnode = myNode.get()
-//    qnode.locked = true
-//    val pred = tail.getAndSet(qnode);
-//    myPred.set(pred)
-//    while (pred.locked) {}
-//  }
-//
-//  override fun lockInterruptibly() {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun tryLock(): Boolean {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun tryLock(time: Long, unit: TimeUnit): Boolean {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun unlock() {
-//    val qnode = myNode.get()
-//    qnode.locked = false
-//    myNode.set(myPred.get())
-//  }
-//
-//  override fun newCondition(): Condition {
-//    TODO("Not yet implemented")
-//  }
-//}
-//
-//class MCSLock :Lock {
-//  class QNode {
-//    @Volatile
-//    var locked: Boolean = false
-//    @Volatile
-//    var next: QNode? = null
-//  }
-//  var tail: AtomicReference<QNode> = AtomicReference(null)
-//  var myNode: ThreadLocal<QNode> = ThreadLocal.withInitial { QNode() }
-//  override fun lock() {
-//    val qnode = myNode.get()
-//    val pred = tail.getAndSet(qnode)
-//    if (pred != null) {
-//      qnode.locked = true
-//      pred.next = qnode
-//      while (qnode.locked) {}
-//    }
-//  }
-//
-//  override fun lockInterruptibly() {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun tryLock(): Boolean {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun tryLock(time: Long, unit: TimeUnit): Boolean {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun unlock() {
-//    val qnode = myNode.get()
-//    if (qnode.next == null) {
-//      if (tail.compareAndSet(qnode, null)) {
-//        return
-//      }
-//      while (qnode.next == null) {}
-//    }
-//    qnode.next!!.locked = false
-//    qnode.next = null
-//  }
-//
-//  override fun newCondition(): Condition {
-//    TODO("Not yet implemented")
-//  }
-//}
-//
-//class TOLock : Lock {
-//  class QNode {
-//    @Volatile
-//    var pred: QNode? = null
-//  }
-//
-//  companion object {
-//    val AVAILABLE: QNode = QNode()
-//  }
-//  var tail:AtomicReference<QNode> = AtomicReference(null)
-//  var myNode:ThreadLocal<QNode> = ThreadLocal.withInitial { QNode() }
-//  override fun lock() {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun lockInterruptibly() {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun tryLock(): Boolean {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun tryLock(time: Long, unit: TimeUnit): Boolean {
-//    val startTime = System.currentTimeMillis()
-//    val patience = TimeUnit.MILLISECONDS.convert(time, unit)
-//    val qnode = QNode()
-//    myNode.set(qnode)
-//    qnode.pred = null
-//    var myPred = tail.getAndSet(qnode)
-//    if (myPred == null || myPred.pred == AVAILABLE) {
-//      return true
-//    }
-//    while (System.currentTimeMillis() - startTime < patience) {
-//      val predPred = myPred.pred
-//      if (predPred == AVAILABLE) {
-//        return false
-//      } else if (predPred != null) {
-//        myPred = predPred
-//      }
-//    }
-//    if (!tail.compareAndSet(qnode, myPred))
-//      qnode.pred = myPred
-//    return false
-//  }
-//
-//  override fun unlock() {
-//    val qnode = myNode.get()
-//    if (!tail.compareAndSet(qnode, null))
-//      qnode.pred = AVAILABLE
-//  }
-//
-//  override fun newCondition(): Condition {
-//    TODO("Not yet implemented")
-//  }
-//
-//}
 
 class Counter(private val lock: Lock, private val limit: Int = 1_000_000) {
   var counter = 0
@@ -324,12 +30,10 @@ class Counter(private val lock: Lock, private val limit: Int = 1_000_000) {
         lock.unlock()
       }
     }
-//    (lock as HCLHLock)?.currNode?.get()?.isTailWhenSpliced = true
-//    (lock as HCLHLock)?.currNode?.get()?.isSuccessorMustWait = false
   }
 }
 
-fun testForThreads(numThreads:Int, lock:Lock, limit:Int = 1_000_000, executor: ExecutorService):Long {
+fun testForThreads(numThreads: Int, lock: Lock, limit: Int = 1_000_000, executor: ExecutorService): Long {
   val counter = Counter(lock, limit)
   val start = System.currentTimeMillis()
   val barrier = CyclicBarrier(numThreads + 1)
@@ -342,50 +46,85 @@ fun testForThreads(numThreads:Int, lock:Lock, limit:Int = 1_000_000, executor: E
   }
   barrier.await()
   val end = System.currentTimeMillis()
-  val totalTime =  end - start
+  val totalTime = end - start
   assert(counter.counter == limit)
   return totalTime
 }
 
-fun createPlot(data: Map<String, Any>) : Plot {
-  var plot = letsPlot(data) { x="numThreads"; y="time"; color="name" }
-  plot += geomPoint {color="name"}
-  plot += geomSmooth(method="loess", span=0.5, size=1.0, ymin=0) { color="name"; group="name" }
+fun createPlot(data: Map<String, Any>): Plot {
+  var plot = letsPlot(data) { x = "numThreads"; y = "time"; color = "name" }
+  plot += geomPoint { color = "name" }
+  plot += geomSmooth(method = "loess", span = 0.5, size = 1.0, ymin = 0) { color = "name"; group = "name" }
   return plot
 }
 
-fun main() {
-  val minThreads = 1
-  val maxThreads = 4
+fun main(args: Array<String>) {
+
+  val lockList = listOf<(Int) -> Lock>(
+    { ALock(it) },
+    { BackoffLock() },
+    { CLHLock() },
+    { CompositeLock() },
+    { CompositeFastPathLock() },
+    { HBOLock() },
+    { HCLHLock() },
+    { MCSLock() },
+    { TASLock() },
+    { TTASLock() },
+    { TOLock() },
+  )
+  val lockNames = listOf(
+    "ALock",
+    "BackoffLock",
+    "CLHLock",
+    "CompositeLock",
+    "CompositeFastPathLock",
+    "HBOLock",
+    "HCLHLock",
+    "MCSLock",
+    "TASLock",
+    "TTASLock",
+    "TOLock"
+  )
+  val lockNameFactoryMap = lockNames.zip(lockList).toMap()
+  val lockFactoryNameMap = lockList.zip(lockNames).toMap()
+  println(lockNameFactoryMap)
+  val parser = ArgParser("Locks Experiments")
+  val minThreads =
+    parser.option(ArgType.Int, fullName = "min", shortName = "m", description = "Min number of threads").default(1)
+  val maxThreads =
+    parser.option(ArgType.Int, fullName = "max", shortName = "M", description = "Max number of threads").default(8)
+  val step = parser.option(ArgType.Int, fullName = "step", shortName = "s", description = "Step for threads").default(1)
+  val limit = parser.option(ArgType.Int, fullName = "limit", shortName = "l", description = "Limit for counter")
+    .default(1_000_000)
+  val numTrials =
+    parser.option(ArgType.Int, fullName = "trials", shortName = "t", description = "Number of trials").default(10)
+  val locks = parser.option(
+    ArgType.Choice<(Int) -> Lock>(lockList, { lockNameFactoryMap[it]!! }, { lockFactoryNameMap[it]!! }),
+    fullName = "locks", shortName = "L", description = "Locks to test"
+  ).delimiter(",").default(lockList)
+  val output = parser.option(ArgType.String, fullName = "output", shortName = "o", description = "Output file").default("plot")
+  val outputFormat = parser.option(ArgType.Choice<String>(listOf("png", "pdf", "svg", "jpeg", "tiff", "webp"), { it }, { it }), fullName = "format", shortName = "f", description = "Output format").default("png")
+  parser.parse(args)
   val data = mutableMapOf<String, MutableList<Any>>()
-  for (numThreads in minThreads..maxThreads){
-    for (lockFactory in listOf(
-      { ALock(numThreads) },
-      { BackoffLock() },
-      { CLHLock() },
-      { CompositeLock() },
-      { CompositeFastPathLock() },
-      { HBOLock() },
-      { HCLHLock() },
-      { MCSLock() },
-      { TASLock() },
-      { TTASLock() },
-      { TOLock() },
-    )) {
-      val executor = Executors.newFixedThreadPool(numThreads)
-      repeat(10) {
+  for (numThreads in minThreads.value..maxThreads.value step step.value) {
+    val executor = Executors.newFixedThreadPool(numThreads)
+    for (lockFactory in locks.value) {
+      repeat(numTrials.value) {
         ThreadID.reset()
-        val lock = lockFactory()
+        val lock = lockFactory(numThreads)
         val name = lock.javaClass.simpleName
-        val result = testForThreads(numThreads, lock, 1_000_000, executor)
+        val result = testForThreads(numThreads, lock, limit.value, executor)
         data.getOrPut("time") { mutableListOf() }.add(result)
         data.getOrPut("numThreads") { mutableListOf() }.add(numThreads)
         data.getOrPut("name") { mutableListOf() }.add(name)
         println("Test for $numThreads threads with $name takes $result milliseconds!")
       }
-      executor.shutdownNow()
     }
+    executor.shutdownNow()
   }
+  println("Finish processing!")
   val fig = createPlot(data)
-  ggsave(fig, "plot.png")
+  ggsave(fig, "${output.value}.${outputFormat.value}")
+  println("Finish plotting!")
 }
